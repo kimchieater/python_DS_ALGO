32269303296c9a5c37ec061ab615b398814dba6f
kimchieater <jasonj19276@gmail.com>
2023-05-17 11:48:27 +1000

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -23,7 +23,7 @@ def locate_card(cards, query):
 
 test = {
     'input': {
-        'cards' : {13,11,10,7,4,3,1,0},
+        'cards' : [13,11,10,7,4,3,1,0],
         'query': 7
     },
     'output':3
