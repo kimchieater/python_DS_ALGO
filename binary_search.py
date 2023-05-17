0f7e983540d83130b65e5d1b769b22e8873769a2
kimchieater <jasonj19276@gmail.com>
2023-05-17 11:18:19 +1000

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -16,6 +16,7 @@ test = {
 tests = []
 tests.append(test)
 
+#query occurs in the middle
 tests.append({
     'input': {
         'cards': [13,11,10,7,4,3,1,0],
@@ -23,3 +24,54 @@ tests.append({
     },
     'output': 6
 })
+
+#query is the first element
+tests.append({
+    'input':{
+        'cards': [4,2,1,-1],
+        'query': 1
+    },
+    'output':0
+})
+#query is the last element
+tests.append({
+    'input': {
+        'cards':[3, -1, -9, -127],
+        'query': -127
+    },
+    'output': 3
+})
+#cards just coins just one elelment, query
+tests.append({
+    'input': {
+        'cards':[6],
+        "query": 6
+    },
+    'output': 0
+})
+#card does not contain query
+tests.append({
+    'input':{
+        'cards': [9,7,5,2,-9],
+        'query': 4
+    },
+    'output': -1
+})
+#card is empty
+tests.append({
+    'input':{
+        'cards': [],
+        'query': 7
+    },
+    'output': -1
+})
+#numbers can repeat in cards
+tests.append({
+    "input":{
+        'cards':[8,8,6,6,6,6,6,3,2,2,2,0,0,0],
+        'query': 3
+    },
+    "output": 7
+})
+
+print(tests)
